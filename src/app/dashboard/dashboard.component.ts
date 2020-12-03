1517089c106ac93ac91483a51551c380a6f7d01a
Misha Krysko <rebel@MacBook-Pro-Misha.local>
2020-12-03 16:27:31 +0200

    Get data from a server

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes.slice(2, 6));
   }
 }
